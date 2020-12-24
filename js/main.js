18b8ed64feef8fe8c4873dae0b075b571f965ea3
cocorobarestudio <fujiwara@cocorobare.com>
2020-12-25 08:18:48 +0900

    25

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@
     const btn = document.getElementById('btn');
 
     btn.addEventListener('click', () => {
-        const results = ['大吉', '中吉', '凶', '末吉'];
-        btn.textContent = results[Math.floor(Math.random() * results.length)];
+        // const results = ['大吉', '中吉', '凶', '末吉'];
+        // btn.textContent = results[Math.floor(Math.random() * results.length)];
         // btn.textContent = 'hit!';
         // switch (n){
         //     case 0:
@@ -19,6 +19,16 @@
         //         break;
 
         // }
+
+
+        const n = Math.random();
+        if (n < 0.05){
+            btn.textContent = '大吉';
+        } else if (n < 0.2){
+            btn.textContent = '中吉';
+        } else {
+            btn.textContent = '凶';
+        }
 
     });
 }
